ee5b1c7cb373dc7f6fded30db52e6f9cb5cf8941
Francesco Ciocchetti <fciocchetti@mintel.com>
2018-10-16 11:30:10 +0100

    Rework entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-set -x
+
+set -ex
+
 KUBERNETES_SERVICE_HOST=${KUBERNETES_SERVICE_HOST:?Please specify KUBERNETES_SERVICE_HOST env variable}
 KUBERNETES_SERVICE_PORT_HTTPS=${KUBERNETES_SERVICE_PORT_HTTPS:?Please specify KUBERNETES_SERVICE_PORT_HTTPS env variable}
 
@@ -7,24 +9,43 @@ BASE_URL="https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT_HTTPS}/ap
 CA_CERT="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 
-trap 'kill -TERM $PID $CURL_PID' TERM INT
-while true; do
-  # curl exits from time to time, most probably because of kube-proxy reloads iptables rules, needs deeper investigation. As for now, let's use a loop trick.
-  RESOURCE_VERSION=$(curl -s "${BASE_URL}/events" --cacert "${CA_CERT}" -H "Authorization: Bearer ${TOKEN}" | jq -r '.metadata.resourceVersion')
-  DATE=$(date --utc +"%Y-%m-%dT%TZ")
-  echo "{\"time\":\"${DATE}\",\"object\":{\"message\":\"Monitoring Kubernetes events staring from ${RESOURCE_VERSION} resourceVersion\"}}" >&2
-
-  if [ "x$OUTPUT_MODE" == "xstdout" ]; then
-    curl -s "${BASE_URL}/watch/events?resourceVersion=${RESOURCE_VERSION}" --cacert "${CA_CERT}" -H "Authorization: Bearer ${TOKEN}" &
-    PID=$!
-    CURL_PID=""
-  elif [ "x$OUTPUT_MODE" == "xnetcat" ]; then
-    ( curl -s "${BASE_URL}/watch/events?resourceVersion=${RESOURCE_VERSION}" --cacert "${CA_CERT}" -H "Authorization: Bearer ${TOKEN}" & echo $! > /tmp/curl.pid) | nc $NETCAT_DST_HOST $NETCAT_DST_PORT &
-    PID=$!
-    CURL_PID=$(cat /tmp/curl.pid)
-  else
-    echo "Unsupported mode $OUTPUT_MODE" >&2
-    exit 1
+# SIGTERM-handler
+term_handler() {
+  if [ $PID -ne 0 ]; then
+    set +e
+    kill -15 "$PID" # SIGTERM
+    wait "$PID"
+    set -e
   fi
-  wait $PID
+  if [ $CURL_PID -ne 0 ] && [ $CURL_PID != "" ]; then
+    set +e
+    kill -15 "$CURL_PID" # SIGTERM
+    wait "$CURL_PID"
+    set -e
+  fi
+  exit 0;
+  #exit 143; # 128 + 15 -- SIGTERM
+}
+
+
+# Trap the TERM Signals
+trap 'kill ${!}; term_handler' SIGTERM
+
+RESOURCE_VERSION=$(curl -s "${BASE_URL}/events" --cacert "${CA_CERT}" -H "Authorization: Bearer ${TOKEN}" | jq -r '.metadata.resourceVersion')
+
+if [ "x$OUTPUT_MODE" == "xstdout" ]; then
+  curl -s "${BASE_URL}/watch/events?resourceVersion=${RESOURCE_VERSION}" --cacert "${CA_CERT}" -H "Authorization: Bearer ${TOKEN}" &
+  PID=$!
+  CURL_PID=""
+elif [ "x$OUTPUT_MODE" == "xnetcat" ]; then
+  ( curl -s "${BASE_URL}/watch/events?resourceVersion=${RESOURCE_VERSION}" --cacert "${CA_CERT}" -H "Authorization: Bearer ${TOKEN}" & echo $! > /tmp/curl.pid) | nc $NETCAT_DST_HOST $NETCAT_DST_PORT &
+  PID=$!
+  CURL_PID=$(cat /tmp/curl.pid)
+else
+  echo "Unsupported mode $OUTPUT_MODE" >&2
+  exit 1
+fi
+
+while true ; do
+   tail -f /dev/null & wait ${!}
 done
