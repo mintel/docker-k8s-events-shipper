5e716afafde834c68a7450ce5a3818996ceb0a5f
Francesco Ciocchetti <fciocchetti@mintel.com>
2018-10-16 16:11:00 +0100

    add watch query parameter

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,10 +12,10 @@ TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 RESOURCE_VERSION=$(curl -s "${BASE_URL}/events" --cacert "${CA_CERT}" -H "Authorization: Bearer ${TOKEN}" | jq -r '.metadata.resourceVersion')
 
 if [ "x$OUTPUT_MODE" == "xstdout" ]; then
-  curl -s "${BASE_URL}/watch/events?resourceVersion=${RESOURCE_VERSION}" --cacert "${CA_CERT}" -H "Authorization: Bearer ${TOKEN}"
+  curl -s "${BASE_URL}/watch/events?resourceVersion=${RESOURCE_VERSION}&watch=true" --cacert "${CA_CERT}" -H "Authorization: Bearer ${TOKEN}"
 elif [ "x$OUTPUT_MODE" == "xnetcat" ]; then
   /wait-for-it.sh -h $NETCAT_DST_HOST -p $NETCAT_DST_PORT -t 60
-  curl -s "${BASE_URL}/watch/events?resourceVersion=${RESOURCE_VERSION}" --cacert "${CA_CERT}" -H "Authorization: Bearer ${TOKEN}" | nc $NETCAT_DST_HOST $NETCAT_DST_PORT
+  curl -s "${BASE_URL}/watch/events?resourceVersion=${RESOURCE_VERSION}&watch=true" --cacert "${CA_CERT}" -H "Authorization: Bearer ${TOKEN}" | nc $NETCAT_DST_HOST $NETCAT_DST_PORT
 else
   echo "Unsupported mode $OUTPUT_MODE" >&2
   exit 1
